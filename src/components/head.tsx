12b76434a05a69e3acd5dadfd0fd97bfa6f61c28
Himansa Eshan <himansa.24067@gmail.com>
2023-06-21 19:21:25 +0530

    added icons

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -13,6 +13,31 @@ export default function Header() {
         content="Ideanix, SLTC, student community, empowering students, innovative ideas, resources and guidance, Ideathon, Designathon, Hackathon, industry-level mentoring, real-world experience, startups, IT industry, TED talks, innovation, creativity, success, empowerment, Sri Lanka"
       />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="og:title" content="IDEANIX: Where ideas become reality." />
+      <meta
+        name="og:description"
+        content="The IEEE Computer Society Student Branch Chapter of SLTC is proud to present for the first time in its history - Ideanix, a project aimed at empowering and inspiring the student community with innovative ideas."
+      />
+      <meta name="og:image" content="/logo-x.webp" />
+      <link rel="icon" href="/favicon-96x96.png" type="image/png" />
+      <link
+        rel="icon"
+        href="/favicon-16x16.png"
+        sizes="16x16"
+        type="image/png"
+      />
+      <link
+        rel="icon"
+        href="/favicon-32x32.png"
+        sizes="32x32"
+        type="image/png"
+      />
+      <link
+        rel="icon"
+        href="/favicon-96x96.png"
+        sizes="96x96"
+        type="image/png"
+      />
     </Head>
   );
 }
